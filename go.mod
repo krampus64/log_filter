079e7e69d8981733a7b3320073654dad276925b7
Milos Radic <milos.radic@htecgroup.com>
2021-04-09 17:49:44 +0200

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/krampus64/log_filter
+
+go 1.15
---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"time"
+)
+
+func main() {
+
+	file := flag.String("file", "", "origin file that need to be filtered")
+
+	flag.Parse()
+	line := flag.Args()
+	if len(*file) == 0 {
+		fmt.Println("Usage: log_filter.go -file")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	logToFilter := *file
+	lineToFilter := line
+
+	saveFileName := logToFilter + "-FILTERED-" + time.Now().Format(time.StampMilli)
+	fmt.Println(lineToFilter)
+
+	var scanner *bufio.Scanner
+
+	openedFile, _ := os.Open("./" + logToFilter)
+	scanner = bufio.NewScanner(openedFile)
+	for scanner.Scan() {
+
+		currentLine := scanner.Text()
+		for _, lineToFilter := range line {
+			if stringContains(currentLine, lineToFilter) {
+				appendToLog(currentLine, saveFileName)
+				fmt.Println(currentLine)
+			}
+		}
+
+	}
+}
+
+func stringContains(scan, value string) bool {
+	doesContain := strings.Contains(scan, value)
+	return doesContain
+}
+
+// This func will make file if doesn't exist, if does it will append log data to it
+func appendToLog(logLine, logFile string) {
+
+	f, err := os.OpenFile("./"+logFile+".log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if _, err := f.Write([]byte(logLine + "\n")); err != nil {
+		log.Fatal(err)
+	}
+	if err := f.Close(); err != nil {
+		log.Fatal(err)
+	}
+}
